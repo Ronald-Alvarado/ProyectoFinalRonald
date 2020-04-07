0108c5c86e0cd64ee452455b04f732197625e2d3
Ronald Alvarado Taberas <ronald_alvarado@ucne.edu.do>
2020-04-07 19:21:36 -0400

    Add project files.

---
+++ ProjectoFinalRonald/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjectoFinalRonald
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //-----------------------------Registros-----------------------//
+
+        private void RegistrarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarProductos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegistrarSalidas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        //----------------------Consultas-------------------------------//
+
+        private void ConsultarUsuario_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarProductos_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ConsultarSalidas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
